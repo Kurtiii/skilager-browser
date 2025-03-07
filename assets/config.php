62dcf406804326eb7c6861e1705b6a3c95a301e2
Kurt Krüger <hi@kurtiii.de>
2025-03-07 16:46:57 +0100

    Erster Commit

--- assets/config.php
+++ assets/config.php
@@ -5,8 +5,16 @@ $_CONFIG['debug'] = true; // Set to true to enable debug mode
 
 if ($_CONFIG['production']) {
     $_CONFIG['base_url'] = 'https://';
+    $db_host = 'localhost';
+    $db_name = '';
+    $db_user = '';
+    $db_password = '';
 } else {
-    $_CONFIG['base_url'] = 'http://localhost/skilager-browser/';
+    $_CONFIG['base_url'] = 'http://localhost/';
+    $db_host = 'localhost';
+    $db_name = '';
+    $db_user = 'root';
+    $db_password = '';
 }
 
 if ($_CONFIG['debug']) {
@@ -18,3 +26,5 @@ if ($_CONFIG['debug']) {
     ini_set('display_startup_errors', 0);
     error_reporting(0);
 }
+
+$_PDO = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
